d526854b4f224157eeb50f90a0e76b1d88ed6b4c
Billy Purvis <purvisbilly@outlook.com>
2015-11-23 18:54:14 +0000

    Updated ajax req

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 /*jslint browser: true*/
 /*global $, jQuery*/
+
 $(function () {
     'use strict';
     var Geo = {},
@@ -18,33 +19,33 @@ $(function () {
 
         $.ajax({
             url: "http://api.wunderground.com/api/0e5af2c42173a4e4/geolookup/conditions/q/" + Geo.lat + "," + Geo.lng + ".json",
+        
             dataType: "jsonp", // JSON can't exchange data between servers, so we use JSONP.
-            success: function (parsed_json) {
-                var location = parsed_json.location.city,
-                    temp_c   = parsed_json.current_observation.temp_c,
-                    temp_f   = parsed_json.current_observation.temp_f,
-                    wind_mph = parsed_json.current_observation.wind_mph,
-                    weather  = parsed_json.current_observation.weather,
-                    img      = parsed_json.current_observation.icon_url;
-                console.log(parsed_json);
+            success: function (Data) {
+                var location = Data.location.city,
+                    temp_c   = Data.current_observation.temp_c,
+                    temp_f   = Data.current_observation.temp_f,
+                    wind_mph = Data.current_observation.wind_mph,
+                    weather  = Data.current_observation.weather,
+                    img      = Data.current_observation.icon_url;
+                console.log(Data);
                 // Bind data to HTML Elms
-                document.getElementById("location").innerHTML = location;
-                document.getElementById("weather").innerHTML  = weather;
-                document.getElementById("temp").innerHTML     = temp_c + " c" + " / " + temp_f + " f";
-                document.getElementById("wind").innerHTML     = wind_mph + " mph";
+                $("#location").html(location);
+                $("#weather").html(weather);
+                $("#temp").html(temp_c + " c" + " / " + temp_f + " f");
+                $("#wind").html(wind_mph + " mph");
                 $("img").attr('src', img);
-
-            }
+            },
+            error: function () {
+                $("#err").html("Error");
+            };
         });
     }
 
     function populateCoords(lat, lng) {
         // If you want to show co-ords
     }
-
-    function error() {
-        document.getElementById("err").innerHTML = "Geo Location Failed, either your browser doesn't support Geo Location or it's blocked.";
-    }
-
+    
+    // Err function previously here, but I'm refurned an error the sucess doesn't respond. 
 
 });
