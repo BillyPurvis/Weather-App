dcbe2f07af8181f62367f6d27eb5fdaeb7e60246
Billy Purvis <purvisbilly@outlook.com>
2015-11-27 20:48:31 +0000

    refactored, caching and I actually understand it

--- scripts.js
+++ scripts.js
@@ -6,10 +6,11 @@ $(function () {
     // Define a options parameter where we can 
     // limit timeOut etc
     var configOpt = {
-        timeout: 2000
+        timeout: 2000,
+        cache: true
     };
     
-    // Get position and set getWeather() as a parameter if successful
+    // Get position and invoke location and ajax req if successful location grab
     function getLocation() {
         
         var location = $("#location");
@@ -19,13 +20,24 @@ $(function () {
             return;
         }
         
-        function showLocation(position) {
+        function showLocation(position, configOpt) {
+            
             var latitude  = position.coords.latitude,
                 longitude = position.coords.longitude;
             
-            console.log(latitude);
-            console.log(longitude);
-            
+            $.ajax({
+                url: "http://api.wunderground.com/api/0e5af2c42173a4e4/geolookup/conditions/q/" + latitude + "," + longitude + ".json",
+                dataType : "jsonp",
+                success : function (Data) {
+                    var location = Data.location.city,
+                        temp_c   = Data.current_observation.temp_c,
+                        wind_mph = Data.current_observation.wind_mph;
+                    $("#location").html("Current temperature in " + location + " is: " + temp_c + " and wind is " + wind_mph + " mph");
+                },
+                error: function (errorThrown) {
+                    $("#i").html("Weather Err");
+                }
+            });
         }
         
         function LocationError() {
@@ -34,28 +46,4 @@ $(function () {
         navigator.geolocation.getCurrentPosition(showLocation, LocationError);
     }
     getLocation();
-    
-
-    
-    
-    function getWeather() {
-   
-        $.ajax({
-            url : "http://api.wunderground.com/api/0e5af2c42173a4e4/geolookup/conditions/q/GB/London.json",
-            dataType : "jsonp",
-            success : function (Data) {
-                var location = Data.location.city,
-                    temp_c   = Data.current_observation.temp_c,
-                    wind_mph = Data.current_observation.wind_mph;
-                console.log("Current temperature in " + location + " is: " + temp_c + " and wind is " + wind_mph + " mph");
-            },
-            error: function (errorThrown) {
-                console.log("You fucked up boy");
-            }
-        });
-   
-    }
-    getWeather();
-    
-    
 });
